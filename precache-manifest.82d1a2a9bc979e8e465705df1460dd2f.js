7356ba9115317af955a5d336c097a7be3edb062a
Expenses <expenses@airmail.cc>
2019-08-23 12:18:52 +1200

    Updates

---
+++ precache-manifest.82d1a2a9bc979e8e465705df1460dd2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f19f410e5f024294cdd5679cabea93e3",
+    "url": "/caster/index.html"
+  },
+  {
+    "revision": "c0ad27bae0bad59a0cb7",
+    "url": "/caster/static/css/main.58fddce5.chunk.css"
+  },
+  {
+    "revision": "f878a0142515ae9efa14",
+    "url": "/caster/static/js/2.42ba5f3a.chunk.js"
+  },
+  {
+    "revision": "c0ad27bae0bad59a0cb7",
+    "url": "/caster/static/js/main.b6100570.chunk.js"
+  },
+  {
+    "revision": "28feef49a4cdb0589caf",
+    "url": "/caster/static/js/runtime~main.48bc4c78.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/caster/precache-manifest.17f28d65acdb8274bf2faf1689fa5b17.js"
+  "/caster/precache-manifest.82d1a2a9bc979e8e465705df1460dd2f.js"
 );
 
 self.addEventListener('message', (event) => {
