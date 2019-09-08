97f2b832fa9c42a1ad6bdd21d87a62e7e14a6227
Expenses <expenses@airmail.cc>
2019-09-08 13:50:10 +1200

    added better favicons and manifest keys

---
+++ makeicons.sh
@@ -0,0 +1,3 @@
+convert -background none public/favicon.svg -define icon:auto-resize public/favicon.ico
+convert -background none public/favicon.svg -resize 512x512 public/favicon_512.png
+convert -background none public/favicon.svg -resize 192x192 public/favicon_192.png
